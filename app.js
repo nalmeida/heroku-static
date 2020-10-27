63f9bfb966f95ccb99a1563bbb0f5adddd1e3ec8
nalmeida <nicholas.almeida@sapientag2.com.br>
2020-10-27 17:19:32 -0300

    Comando de deploy + rota /

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express();
+
 app.listen(port, function(){
 	console.log(`Server started on port ${port}`);
+});
+
+app.get('/', function(req, res) {
+  res.send('<h1> Hello heroku-static!')
 });
