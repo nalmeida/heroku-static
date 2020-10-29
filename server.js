0006a1a3f74079019809880ce4ee346b1da9e1be
nalmeida <nicholas.almeida@sapientag2.com.br>
2020-10-29 17:37:04 -0300

    Added the helmet for server protection

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(helmet());
 
 // @source: https://medium.com/javascript-in-plain-english/add-basic-authentication-to-an-express-app-9536f5095e88
 app.use(basicAuth({
